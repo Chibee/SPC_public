3870d7145c0982d14d33dade819f43b6ea3abf52
Chibee <cblinqi@gmail.com>
2024-07-16 10:08:18 +0900

    edit shell script

--- Scripts/DCNN/submit_job.sh
+++ Scripts/DCNN/submit_job.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 #-------slurm option--------#
-#SBATCH --partition=mpc
-#SBATCH --account=RB230049
 #SBATCH --nodes=1
-#SBATCH --time=23:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem=8G
-#SBATCH --array=1-222
+#SBATCH --array=1-1000
 
 python -u Calc_dist.py $SLURM_ARRAY_TASK_ID 5
 
--- Scripts/SPC/submit_job.sh
+++ Scripts/SPC/submit_job.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 #-------slurm option--------#
-#SBATCH --partition=mpc
-#SBATCH --account=RB230049
 #SBATCH --nodes=1
-#SBATCH --time=23:00:00
+#SBATCH --time=4:00:00
 #SBATCH --mem=90G
 
 python -u Train_SPC_original.py 7
